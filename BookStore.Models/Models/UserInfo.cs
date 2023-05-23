79fc8b5e1812fb301720bb2aa33dc83c9a30c63a
NikolaiRsv <nikolairusev14@gmail.com>
2023-05-23 17:43:13 +0300

    add jwt

--- BookStore.Models/Models/UserInfo.cs
+++ BookStore.Models/Models/UserInfo.cs
@@ -2,10 +2,10 @@
 {
     public class UserInfo
     {
-        public Guid Id  { get; set; }   
+        public Guid Id { get; set; }
 
-        public string Username { get; set; }    
+        public string Username { get; set; }
 
-        public string Password { get; set; }    
+        public string Password { get; set; }
     }
 }
---
+++ BookStore.Models/Requests/AddUserInfoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace BookStore.Models.Requests
+{
+    public class AddUserInfoRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
